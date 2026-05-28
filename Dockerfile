96c896c420f191d21654435dbefec4b642596d07
Calvinoker <calvinoker@gmail.com>
2026-05-28 17:25:46 +0300

    yes created dokerfile and docker--compose.yml

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,18 @@
 FROM python:3.11
 
+ENV PYTHONDONTWRITEBYTECODE 1
+ENV PYTHONUNBUFFERED 1
+
 WORKDIR /app
 
-COPY requirements.txt .
+# install dependencies
+COPY requirements.txt /app/
+RUN pip install --upgrade pip
+RUN pip install -r requirements.txt
 
-RUN pip install --no-cache-dir -r requirements.txt
+# copy project
+COPY . /app/
 
-COPY . .
+EXPOSE 8000
 
 CMD ["python", "manage.py", "runserver", "0.0.0.0:8000"]
--- config/celery.py
+++ config/celery.py
@@ -1,17 +1,13 @@
 import os
-
 from celery import Celery
 
-os.environ.setdefault(
-    'DJANGO_SETTINGS_MODULE',
-    'config.settings'
-)
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 app = Celery('config')
 
-app.config_from_object(
-    'django.conf:settings',
-    namespace='CELERY'
-)
+app.config_from_object('django.conf:settings', namespace='CELERY')
 
 app.autodiscover_tasks()
+
+app.conf.broker_url = 'redis://redis:6379/0'
+app.conf.result_backend = 'redis://redis:6379/1'
